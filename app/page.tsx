c4b2b94142dbacdf0d6c9ce92457f4c2153af312
hazichomwarr <batchiy6@gmail.com>
2026-07-13 16:08:32 -0400

    feat: remove navbar from admin dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryGrid from "@/components/CategoryGrid";
 import FeaturedProducts from "@/components/FeaturedProducts";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/layout/Navbar";
 import WhyUs from "@/components/WhyUs";
 
 export const dynamic = "force-dynamic";
@@ -8,6 +9,7 @@ export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <main>
+      <Navbar />
       <Hero />
       <CategoryGrid />
       <FeaturedProducts />
